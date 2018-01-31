692925a77dad4670cdf8b275b1ae348e4cfeb09c
alexr101 <alexonezero@outlook.com>
2018-01-31 14:51:56 -0500

    remove old config levelbuilder properties

--- Game/Config.lua
+++ Game/Config.lua
@@ -5,9 +5,6 @@ local config = {
     file = 'level1.json',
     createModes = { 'inGrid', 'outOfGrid' },
   },
-  levelBuilder = false,
-  levelBuilderFile = 'level1.json',
-  levelBuilderCreateModes = {'inGrid'},
   gridData = 'json', -- json, mock, random
   levelSpeed = 0,
   tiles = 6,
